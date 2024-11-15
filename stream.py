0bbdae7e2c5ad3f1897c16869629b2c776434dc5
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-15 15:09:12 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -130,7 +130,7 @@ list_bulan = [
         
 bulan = st.selectbox("MONTH:", list_bulan, index=9, on_change=reset_button_state)
 
-st.markdown('## LEADTIME INTERNAL')
+st.markdown('### Leadtime-Internal')
 pic = st.selectbox("PIC RESPONSIBLE:", ['All','WH/DC','Resto'], index=0, on_change=reset_button_state)
 bulan = bulan[:3]+'-24'
 df_internal['Rute Global'] = pd.Categorical(df_internal['Rute Global'],['WH/DC to WH/DC','WH/DC to Resto','Resto to WH/DC','Resto to Resto'])
