4b51a2ff9c2acb1a6ccd59bb83a3e77c9bd4d9d7
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-15 14:40:34 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -152,7 +152,7 @@ with col[3]:
     st.write('')
     col2 = st.columns(3)
     with col2[0]:
-        st.metric(label="Total", value="{:,.0f}".format(df_internal[(df_internal['Bulan Terima']==bulan) & (df_internal['Terima #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))]['Nomor IT Terima'].nunique()), delta=None)
+        st.metric(label="Total", value="{:,.0f}".format(df_internal[(df_internal['Bulan Kirim']==bulan) & (df_internal['Kirim #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))]['Nomor IT Kirim'].nunique()), delta=None)
     with col2[1]:
         st.metric(label="On-Time", value="{:,.0f}".format(df_pie[df_pie['Kategori Leadtime SJ']=='On-Time']['Nomor IT Kirim'].values[0]), delta=None)
     with col2[2]:
@@ -179,6 +179,15 @@ with col[2]:
              & (df_internal['Kategori Leadtime RI']=='Backdate')].groupby(['Kirim #2'])[['Nomor IT Terima']].nunique().reset_index()
     create_percentage_barchart(df_bar, 'Kirim #2', 'Nomor IT Terima')
 with col[3]:
+    st.write('')
+    col2 = st.columns(3)
+    with col2[0]:
+        st.metric(label="Total", value="{:,.0f}".format(df_internal[(df_internal['Bulan Terima']==bulan) & (df_internal['Terima #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))]['Nomor IT Terima'].nunique()), delta=None)
+    with col2[1]:
+        st.metric(label="On-Time", value="{:,.0f}".format(df_pie[df_pie['Kategori Leadtime SJ']=='On-Time']['Nomor IT Terima'].values[0]), delta=None)
+    with col2[2]:
+        st.metric(label="Backdate", value="{:,.0f}".format(df_pie[df_pie['Kategori Leadtime SJ']=='Backdate']['Nomor IT Terima'].values[0]), delta=None)
+        
     st.dataframe(df_internal[(df_internal['Bulan Terima']==bulan) & (df_internal['Terima #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))
              & (df_internal['Kategori Leadtime RI']=='Backdate')].groupby(['Leadtime RI Group','Rute Global'])[['Nomor IT Terima']].nunique().reset_index().pivot(index='Rute Global',columns='Leadtime RI Group',values='Nomor IT Terima').reset_index(),
                  hide_index=True
