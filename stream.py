489c79f1172825460e580eaff77535817bdea358
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-15 12:44:12 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -77,7 +77,7 @@ def create_pie_chart(df, labels_column, values_column, title="Pie Chart"):
             x=0.5
         )
     )
-    fig.update_layout(width=600, height=600)
+    fig.update_layout(width=400, height=400)
     # Menampilkan grafik di Streamlit
     st.plotly_chart(fig)
 
