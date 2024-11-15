07ca6473dcccd263b9895f94f23152d6064163bc
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-15 17:15:31 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -222,7 +222,7 @@ with col[2]:
     create_percentage_barchart(df_bar, 'Rute', 'Nomor PO')
     
 
-st.write('Kategori Item: Eksternal Logistic')
+st.write('Kategori Item: Internal Logistic')
 col = st.columns([1,2,1])
 
 with col[0]:
@@ -240,3 +240,20 @@ with col[2]:
              & (df_eksternal['Kategori PO(Datang)-PR(Create)']=='Backdate') & (df_eksternal['Kategori Item']=='Internal Logistic')].groupby(['Rute'])[['Nomor PO']].nunique().reset_index()
     create_percentage_barchart(df_bar, 'Rute', 'Nomor PO')
 
+st.write('Kategori Item: Resto')
+col = st.columns([1,2,1])
+
+with col[0]:
+    df_pie = df_eksternal[(df_eksternal['Bulan PO']==bulan) & (df_eksternal['PIC Responsible']=='Resto') 
+             )].groupby(['Kategori PO(Datang)-PR(Create)'])[['Nomor PO']].nunique().reset_index()
+    create_pie_chart(df_pie, labels_column='Kategori PO(Datang)-PR(Create)', values_column='Nomor PO', title="OUTGOING BACKDATE")
+with col[1]:
+    df_line = df_eksternal[(df_eksternal['Bulan PO']==bulan) & (df_eksternal['PIC Responsible']=='Resto') 
+             & (df_eksternal['Kategori PO(Datang)-PR(Create)']=='Backdate')
+             ].groupby(['Tanggal PO'])[['Nomor PO']].nunique().reset_index()
+    df_line = df_tanggal.merge(df_line,how='left',left_on='Tanggal',right_on='Tanggal PO').fillna(0)
+    create_line_chart(df_line, x_column='Tanggal', y_column='Nomor PO', title="DAILY BACKDATE")
+with col[2]:
+    df_bar = df_eksternal[(df_eksternal['Bulan PO']==bulan) & (df_eksternal['PIC Responsible']=='Resto') 
+             & (df_eksternal['Kategori PO(Datang)-PR(Create)']=='Backdate')].groupby(['Rute'])[['Nomor PO']].nunique().reset_index()
+    create_percentage_barchart(df_bar, 'Rute', 'Nomor PO')
