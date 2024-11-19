7517a8a806e3b9336206b2678d940c06370641b8
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-19 10:00:37 +0700

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -132,71 +132,8 @@ list_bulan = [
         
 bulan = st.selectbox("MONTH:", list_bulan, index=9, on_change=reset_button_state)
 df_tanggal = pd.DataFrame(pd.date_range(start=f'{2024}-{int(pd.to_datetime(f'{bulan}-2024',format='%B-%Y').strftime('%m')):02d}-01', end=f'{2024}-{int(pd.to_datetime(f'{bulan}-2024',format='%B-%Y').strftime('%m')):02d}-28', freq='D'), columns=['Tanggal'])
-
-st.markdown('### Leadtime-Internal')
-pic = st.selectbox("PIC RESPONSIBLE:", ['All','WH/DC','Resto'], index=0, on_change=reset_button_state)
 bulan = bulan[:3]+'-24'
 df_internal['Rute Global'] = pd.Categorical(df_internal['Rute Global'],['WH/DC to WH/DC','WH/DC to Resto','Resto to WH/DC','Resto to Resto'])
-
-
-col = st.columns([1,2,1,2])
-with col[0]:
-    df_pie = df_internal[(df_internal['Bulan Kirim']==bulan) & (df_internal['Kirim #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))].groupby(['Kategori Leadtime SJ'])[['Nomor IT Kirim']].nunique().reset_index()
-    
-    create_pie_chart(df_pie, labels_column='Kategori Leadtime SJ', values_column='Nomor IT Kirim', title="OUTGOING BACKDATE")
-with col[1]:
-    df_line = df_internal[(df_internal['Bulan Kirim']==bulan) & (df_internal['Kirim #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))
-                 & (df_internal['Kategori Leadtime SJ']=='Backdate')].groupby(['Tanggal IT Kirim'])[['Nomor IT Kirim']].nunique().reset_index()
-    
-    create_line_chart(df_line, x_column='Tanggal IT Kirim', y_column='Nomor IT Kirim', title="DAILY BACKDATE")
-with col[2]:
-    df_bar = df_internal[(df_internal['Bulan Kirim']==bulan) & (df_internal['Kirim #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))
-             & (df_internal['Kategori Leadtime SJ']=='Backdate')].groupby(['Kirim #2'])[['Nomor IT Kirim']].nunique().reset_index()
-    create_percentage_barchart(df_bar, 'Kirim #2', 'Nomor IT Kirim')
-with col[3]:
-    st.write('')
-    col2 = st.columns(3)
-    with col2[0]:
-        st.metric(label="Total", value="{:,.0f}".format(df_internal[(df_internal['Bulan Kirim']==bulan) & (df_internal['Kirim #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))]['Nomor IT Kirim'].nunique()), delta=None)
-    with col2[1]:
-        st.metric(label="On-Time", value="{:,.0f}".format(df_pie[df_pie['Kategori Leadtime SJ']=='On-Time']['Nomor IT Kirim'].values[0]), delta=None)
-    with col2[2]:
-        st.metric(label="Backdate", value="{:,.0f}".format(df_pie[df_pie['Kategori Leadtime SJ']=='Backdate']['Nomor IT Kirim'].values[0]), delta=None)
-        
-    st.dataframe(df_internal[(df_internal['Bulan Kirim']==bulan) & (df_internal['Kirim #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))
-             & (df_internal['Kategori Leadtime SJ']=='Backdate')].groupby(['Leadtime SJ Group','Rute Global'])[['Nomor IT Kirim']].nunique().reset_index().pivot(index='Rute Global',columns='Leadtime SJ Group',values='Nomor IT Kirim').reset_index(),
-                 hide_index=True
-    )
-
-
-col = st.columns([1,2,1,2])
-with col[0]:
-    df_pie = df_internal[(df_internal['Bulan Terima']==bulan) & (df_internal['Terima #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))].groupby(['Kategori Leadtime RI'])[['Nomor IT Terima']].nunique().reset_index()
-    
-    create_pie_chart(df_pie, labels_column='Kategori Leadtime RI', values_column='Nomor IT Terima', title="OUTGOING BACKDATE")
-with col[1]:
-    df_line = df_internal[(df_internal['Bulan Terima']==bulan) & (df_internal['Terima #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))
-                 & (df_internal['Kategori Leadtime RI']=='Backdate')].groupby(['Tanggal IT Terima'])[['Nomor IT Terima']].nunique().reset_index()
-    
-    create_line_chart(df_line, x_column='Tanggal IT Terima', y_column='Nomor IT Terima', title="DAILY BACKDATE")
-with col[2]:
-    df_bar = df_internal[(df_internal['Bulan Terima']==bulan) & (df_internal['Terima #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))
-             & (df_internal['Kategori Leadtime RI']=='Backdate')].groupby(['Terima #2'])[['Nomor IT Terima']].nunique().reset_index()
-    create_percentage_barchart(df_bar, 'Terima #2', 'Nomor IT Terima')
-with col[3]:
-    st.write('')
-    col2 = st.columns(3)
-    with col2[0]:
-        st.metric(label="Total", value="{:,.0f}".format(df_internal[(df_internal['Bulan Terima']==bulan) & (df_internal['Terima #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))]['Nomor IT Terima'].nunique()), delta=None)
-    with col2[1]:
-        st.metric(label="On-Time", value="{:,.0f}".format(df_pie[df_pie['Kategori Leadtime RI']=='On-Time']['Nomor IT Terima'].values[0]), delta=None)
-    with col2[2]:
-        st.metric(label="Backdate", value="{:,.0f}".format(df_pie[df_pie['Kategori Leadtime RI']=='Backdate']['Nomor IT Terima'].values[0]), delta=None)
-        
-    st.dataframe(df_internal[(df_internal['Bulan Terima']==bulan) & (df_internal['Terima #2'].isin(['Resto','WH/DC'] if pic=='All' else [pic]))
-             & (df_internal['Kategori Leadtime RI']=='Backdate')].groupby(['Leadtime RI Group','Rute Global'])[['Nomor IT Terima']].nunique().reset_index().pivot(index='Rute Global',columns='Leadtime RI Group',values='Nomor IT Terima').reset_index(),
-                 hide_index=True
-    )
 
 st.markdown('### Leadtime-Eksternal')
 df_eksternal['Tanggal PO'] = pd.to_datetime(df_eksternal['Tanggal PO'])
@@ -204,33 +141,6 @@ df_eksternal['Tanggal PO'] = pd.to_datetime(df_eksternal['Tanggal PO'])
 import requests
 from streamlit_option_menu import option_menu
 
-# Membuat navigasi bar
-option = option_menu(
-    menu_title="Leadtime ",  # required
-    options=["PO_PR", "PO_PO", "PO_RI"],  # required
-    menu_icon="cast",  # optional
-    default_index=0,  # optional
-    orientation="vertical",
-)
-
-# Fungsi untuk menjalankan file python yang diunduh
-def run_stream_script(url):
-    # Mengunduh file dari GitHub
-    response = requests.get(url)
-    if response.status_code == 200:
-        # Menjalankan file yang diunduh
-        exec(response.text, globals())
-    else:
-        st.error(f"Failed to download file: {response.status_code}")
-
-# Arahkan ke aplikasi berdasarkan pilihan pengguna
-if option == 'PO_PR':
-    stream1_url = 'https://raw.githubusercontent.com/Analyst-FPnA/Leadtime/main/Eksternal_PO_PR.py'
-    run_stream_script(stream1_url)
-  
-elif option == 'PO_PO':
-    stream2_url = 'https://raw.githubusercontent.com/Analyst-FPnA/Leadtime/main/Eksternal_PO_PO.py'
-    run_stream_script(stream2_url)
 
 elif option == 'PO_RI':
     stream3_url = 'https://raw.githubusercontent.com/Analyst-FPnA/Leadtime/main/Eksternal_PO_RI.py'
