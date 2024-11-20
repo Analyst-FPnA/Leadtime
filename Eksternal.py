f1e9b17cbe207e7c21acd5cbc501c523dee9213c
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-20 17:06:52 +0700

    Update Eksternal.py

--- Eksternal.py
+++ Eksternal.py
@@ -7,6 +7,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 st.write('')
+st.markdown('## Summary')
 kat_eksternal = st.selectbox("KATEGORI BACKDATE:", ['PR(Create)-PO(Datang)','PO(Datang)-PO(Create)','PO(Datang)-RI(Create)'], index=0, on_change=reset_button_state)
 
 if kat_eksternal =='PR(Create)-PO(Datang)':
@@ -57,11 +58,13 @@ if kat_eksternal =='PR(Create)-PO(Datang)':
                  & (df_eksternal['Kategori Item']=='Internal Logistic')].groupby(['Kategori PO(Datang)-PR(Create)'])[['Nomor PO']].nunique().reset_index()
         create_pie_chart(df_pie, labels_column='Kategori PO(Datang)-PR(Create)', values_column='Nomor PO', title="OUTGOING BACKDATE")
     with col[1]:
-        df_line = df_eksternal[ (df_eksternal['PIC Responsible']=='Logistic') 
+        df_line = df_eksternal[(df_eksternal['PIC Responsible']=='Logistic') 
+                 & (df_eksternal['Kategori Item']=='Internal Logistic')
+                 ].groupby(['Date PO'])[['Nomor PO']].nunique().reset_index().rename(columns={'Nomor PO':'Total'}).merge(df_eksternal[(df_eksternal['PIC Responsible']=='Logistic') 
                  & (df_eksternal['Kategori PO(Datang)-PR(Create)']=='Backdate') & (df_eksternal['Kategori Item']=='Internal Logistic')
-                 ].groupby(['Tanggal PO'])[['Nomor PO']].nunique().reset_index()
-        df_line = df_tanggal.merge(df_line,how='left',left_on='Tanggal',right_on='Tanggal PO').fillna(0)
-        create_line_chart(df_line, x_column='Tanggal', y_column='Nomor PO', title="DAILY BACKDATE")
+                 ].groupby(['Date PO'])[['Nomor PO']].nunique().reset_index(), how='left')
+        df_line['Nomor PO'] = (df_line['Nomor PO']/df_line['Total'])*100
+        create_line_chart(df_line, x_column='Date PO', y_column='Nomor PO', title="DAILY BACKDATE")
     with col[2]:
         df_bar = df_eksternal[ (df_eksternal['PIC Responsible']=='Logistic') 
                  & (df_eksternal['Kategori PO(Datang)-PR(Create)']=='Backdate') & (df_eksternal['Kategori Item']=='Internal Logistic')].groupby(['Rute'])[['Nomor PO']].nunique().reset_index()
@@ -92,11 +95,12 @@ if kat_eksternal =='PR(Create)-PO(Datang)':
                  ].groupby(['Kategori PO(Datang)-PR(Create)'])[['Nomor PO']].nunique().reset_index()
         create_pie_chart(df_pie, labels_column='Kategori PO(Datang)-PR(Create)', values_column='Nomor PO', title="OUTGOING BACKDATE")
     with col[1]:
-        df_line = df_eksternal[ (df_eksternal['PIC Responsible']=='Resto') 
+        df_line = df_eksternal[(df_eksternal['PIC Responsible']=='Resto') 
+                 ].groupby(['Date PO'])[['Nomor PO']].nunique().reset_index().rename(columns={'Nomor PO':'Total'}).merge(df_eksternal[(df_eksternal['PIC Responsible']=='Resto') 
                  & (df_eksternal['Kategori PO(Datang)-PR(Create)']=='Backdate')
-                 ].groupby(['Tanggal PO'])[['Nomor PO']].nunique().reset_index()
-        df_line = df_tanggal.merge(df_line,how='left',left_on='Tanggal',right_on='Tanggal PO').fillna(0)
-        create_line_chart(df_line, x_column='Tanggal', y_column='Nomor PO', title="DAILY BACKDATE")
+                 ].groupby(['Date PO'])[['Nomor PO']].nunique().reset_index(), how='left')
+        df_line['Nomor PO'] = (df_line['Nomor PO']/df_line['Total'])*100
+        create_line_chart(df_line, x_column='Date PO', y_column='Nomor PO', title="DAILY BACKDATE")
     with col[2]:
         df_bar = df_eksternal[ (df_eksternal['PIC Responsible']=='Resto') 
                  & (df_eksternal['Kategori PO(Datang)-PR(Create)']=='Backdate')].groupby(['Rute'])[['Nomor PO']].nunique().reset_index()
